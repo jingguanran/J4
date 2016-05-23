c28827f2ef5f3e4d5a04d5fe9c02c1e14f1d62e7
jingguanran <jingguanran@126.com>
2016-05-23 09:34:09 +0800

    add new add some new files

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name j4App
+ * @description
+ * # j4App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('j4App', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name j4App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the j4App
+ */
+angular.module('j4App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
